1edc704ff9a0668e9d1e7ed405c95dacb7a2877a
Manuel Selch <manuel@selch.de>
2025-09-15 21:09:43 +0200

    feat: use env for train & deploy

--- deploy.py
+++ deploy.py
@@ -1,13 +1,17 @@
 import pathlib
+import os
+from dotenv import load_dotenv
 from example_policies.robot_deploy.deploy import deploy_policy
 from example_policies.robot_deploy import policy_loader
 
-CHECKPOINT_DIR = pathlib.Path("outputs/<policy_checkpoint_dir>")
-SERVER_ENDPOINT = "localhost:50051"
+load_dotenv()
+
+CHECKPOINT_DIR = os.getenv("CHECKPOINT_DIR")
+SERVER_ENDPOINT = os.getenv("SERVER_ENDPOINT")
 INFERENCE_FREQUENCY_HZ: float = 1.0
 
 policy, cfg = policy_loader.load_policy(CHECKPOINT_DIR)
-print("✅ Policy loaded successfully!")
+print("Policy loaded successfully!")
 print(f"Action level from config: '{cfg.action_level}'")
 
 
--- train.py
+++ train.py
@@ -1,11 +1,13 @@
-import pathlib
+import os
+from dotenv import load_dotenv
 from example_policies.config_factory import act_config, diffusion_config, smolvla_config
 from example_policies import lerobot_patches
 from example_policies.train import train
 
 lerobot_patches.apply_patches()
+load_dotenv()
 
-DATA_DIR = pathlib.Path("../data/my_awesome_dataset")
+DATA_DIR = os.getenv("DATA_DIR")
 
 cfg = act_config(DATA_DIR)
 
