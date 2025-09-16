7dff5e1615a7be1ab57c18a1e24833acd94a35d7
Manuel Selch <manuel@selch.de>
2025-09-16 18:19:16 +0200

    feat: main entry file

--- main.py
+++ main.py
@@ -5,9 +5,38 @@ load_dotenv()
 
 RAW_DATA_DIR = os.getenv("RAW_DATA_DIR")
 OUTPUT_DIR = os.getenv("OUTPUT_DIR")
-TASK_LABEL = "task-label"
 CHECKPOINT_DIR = os.getenv("CHECKPOINT_DIR")
 
 robot = Robot()
 
-robot.replay(OUTPUT_DIR, episode=0)
+def grapPlateA():
+    robot.deploy(CHECKPOINT_DIR)
+    pass
+
+def placePlateA():
+    robot.replay(OUTPUT_DIR, episode=0)
+
+def grapPlateB():
+    robot.deploy(CHECKPOINT_DIR)
+    pass
+
+def placePlateB():
+    robot.replay(OUTPUT_DIR, episode=1)
+
+def grapScrew():
+    robot.deploy(CHECKPOINT_DIR)
+    pass
+
+def screw():
+    robot.replay(OUTPUT_DIR, episode=2)
+
+def main():
+    grapPlateA()
+    placePlateA()
+    grapPlateB()
+    placePlateB()
+    grapScrew()
+    screw()
+
+# main()
+placePlateA()
