a7ff00df34b3d7a0b74920d7e9de70a6c200b68e
Manuel Selch <manuel@selch.de>
2025-09-19 08:45:02 +0200

    feat: filter only small controus (small yellow circles)     MAX_AREA = 100

--- src/robot/pre_marking.py
+++ src/robot/pre_marking.py
@@ -33,39 +33,15 @@ def mark_frame(frame):
     # skip if no contours found
     if not contours: return frame
 
+    # filter only small controus (small yellow circles)
+    MAX_AREA = 100
+    contours = [c for c in contours if cv2.contourArea(c) < MAX_AREA]
+
+    if not contours: return frame
+
     # ROI = box around yellow colors
     c = max(contours, key=cv2.contourArea)
-    x, y, w, h = cv2.boundingRect(c)
-
     cv2.drawContours(frame, [c], -1, (0, 0, 255), 10)
-
-    # crop ROI
-    roi = frame[y:y+h, x:x+w]
-
-    # convert ROI to to gray
-    gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-    blur = cv2.medianBlur(gray, 5)
-
-    # detect circles
-    circles = cv2.HoughCircles(
-        blur,
-        cv2.HOUGH_GRADIENT,
-        dp=1.2,
-        minDist=1,
-        param1=5,
-        param2=30,
-        minRadius=2,
-        maxRadius=20
-    )
-
-    # skip if no circles found
-    if circles is None: return frame
-
-    # mark circles
-    # circles = np.uint16(np.around(circles))
-    # for (cx, cy, r) in circles[0, :]:
-    #    cv2.circle(frame, (x + cx, y + cy), r, (0, 255, 0), 50)
-
     return frame
 
 # step 1: load each frame of video
